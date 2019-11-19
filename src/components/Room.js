cb95d8bba80a93b05a7313460f38dde96d6bede4
sssgordon <gso7@uwo.ca>
2019-11-19 17:52:34 +0100

    End of day push

--- src/components/Room.js
+++ src/components/Room.js
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
 export default function Room(props) {
+  const list =
+    props.users && props.users.length
+      ? props.users.map((user, index) => <p key={index}>{user.name}</p>)
+      : "This room has no users";
+
   return (
     <Fragment>
       <h2>{props.name}</h2>
       <Button type="submit" onClick={props.onClick}>
         Join
       </Button>
+      {list}
       <p>
         <Link to="/">Return</Link>
       </p>
--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import Room from "./Room";
 import { connect } from "react-redux";
 import { join } from "../actions/join";
+import { Link } from "react-router-dom";
 
 export class RoomContainer extends Component {
   onClick = event => {
@@ -10,12 +11,31 @@ export class RoomContainer extends Component {
   };
 
   render() {
-    return <Room onClick={this.onClick} name={this.props.match.params.name} />;
+    const { name } = this.props.match.params;
+    const { rooms } = this.props;
+    console.log("rooms test:", rooms);
+
+    const room = rooms.find(room => (room.name = name)); // this returns the room we are currently in
+    // you must be logged in
+    console.log("room test", room);
+
+    if (!room) {
+      return (
+        <Fragment>
+          <p>This room does not exist</p>
+          <Link to="/">Return</Link>
+        </Fragment>
+      );
+    }
+
+    const { users } = room;
+
+    return <Room onClick={this.onClick} name={name} users={users} />;
   }
 }
 
 const mapStateToProps = state => {
-  return { jwt: state.user };
+  return { jwt: state.user, rooms: state.rooms };
 };
 
 export default connect(mapStateToProps, { join })(RoomContainer);
